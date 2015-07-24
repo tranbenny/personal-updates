cf848705b5fb802fd91651ac8463708c6d7a008e
tranbenny <tran.benny123@gmail.com>
2015-07-24 14:00:10 -0700

    configured bootstrap and jquery static route

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -22,3 +25,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.load_home), 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_roote = settings.STATIC_ROOT)
