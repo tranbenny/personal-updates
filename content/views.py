83288d68d436ac352d889fd614529d6460f86ced
tranbenny <tran.benny123@gmail.com>
2015-07-24 13:23:08 -0700

    configured get request for nytimes books

--- content/views.py
+++ content/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import get_object_or_404, render
+import requests
+import json
 
 
 # Create your views here.
 def list(request):
+	nyTimesAPI = "http://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=8340b1a97466c874731b753cd607a32e%3A7%3A72549146"
+	titles = requests.get(nyTimesAPI)
+	info = titles.json()
+	results = info["results"]
+	print("Published date: " + results["published_date"])
+	listOfBestSellers = results["lists"] #array
+	allBooks = []
+	for lists in listOfBestSellers: 
+		if (lists["list_name"] == "Combined Print and E-Book Nonfiction"):
+			allBooks = lists["books"]
+			break
+	for book in allBooks:
+		print("\n")
+		print("Title: " + book["title"])
+		print("Author: " + book["author"])	
 	return render(request, "updates/updates_list.html")
